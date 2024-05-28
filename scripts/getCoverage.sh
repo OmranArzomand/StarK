c0f1a037eb185fe5292ed27926c474487cbad0ab
root <root@cloud-vm-41-253.doc.ic.ac.uk>
2024-05-28 20:36:15 +0100

    Use filtered compiler jar for coverage information

--- scripts/getCoverage.sh
+++ scripts/getCoverage.sh
@@ -11,12 +11,15 @@ HTML_OUTPUT_DIR="$3"
 
 export JAVA_OPTS="-javaagent:../kover/kover-jvm-agent-0.8.0.jar=file:../kover/agent.args" 
 
+rm ../output/tmp/kover-report.ic
+
 for file in "$PROG_DIR"/*; do
   java $JAVA_OPTS -jar "$COMPILER_DIR/kotlinc/lib/kotlin-compiler.jar" "$file" -include-runtime -d ../output/tmp/hello.jar
 done
 
+rm -rf "$HTML_OUTPUT_DIR"
 mkdir "$HTML_OUTPUT_DIR"
 
-java -jar ../kover/kover-cli-0.8.0.jar report ../output/tmp/kover-report.ic --classfiles "$COMPILER_DIR/kotlinc/lib/kotlin-compiler.jar" --src ../output/tmp --html "$HTML_OUTPUT_DIR"
+java -jar ../kover/kover-cli-0.8.0.jar report ../output/tmp/kover-report.ic --classfiles "$COMPILER_DIR/kotlinc/lib/filtered-kotlin-compiler.jar" --src ../output/tmp --html "$HTML_OUTPUT_DIR"
 
 
